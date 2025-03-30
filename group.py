c41a1ca4f6b7799ecb97c45b1a6ebd4f54df5617
Tealina28 <104749201+WoodsTide@users.noreply.github.com>
2025-03-30 11:27:40 +0800

    refactor(group): 重构 read函数以获取更多信息
    - 更新 SQL 查询以选择更多列，包括 "40027", "40030", "40033" 和 "40093"
    - 修改映射逻辑，使用 row[2]作为键，若未找到则使用 row[6] 作为默认值

--- group.py
+++ group.py
@@ -12,9 +12,9 @@ def parse(param):
 def read(cursor, mapping):
 
     params = [
-        (row[4], row[6], mapping.get(row[2],None), row[3])
-        for row in cursor.execute('SELECT "40011","40012","40020","40021","40050","40090","40800" FROM group_msg_table ORDER BY "40050"')
-        if row[6]
+        (row[7], row[10], mapping.get(row[2],row[6]), row[3] or row[4] or row[5])
+        for row in cursor.execute('SELECT "40011","40012","40020","40021","40027","40030","40033","40050","40090","40093","40800" FROM group_msg_table ORDER BY "40050"')
+        if row[10]
     ]
 
     return params
