3b127ace5b34ae53da060979dd6ba11bc8e712de
Tealina <104749201+Tealina28@users.noreply.github.com>
2025-05-11 12:35:30 +0800

    feat(main): 添加命令行参数解析功能
    - 使用 argparse 模块替换原有的 sys.argv 解析方式
    - 增加了 --c2c 和 --group 参数，用于指定需要输出的私聊和群聊消息

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import logging
+import argparse
 from pathlib import Path
-from sys import argv
-
 from tqdm import tqdm
 
 import db
+
+parser = argparse.ArgumentParser(description="读取并导出解密后的QQNT数据库中的聊天记录")
+
+parser.add_argument("path", type=str, help="解密后的数据库目录路径")
+parser.add_argument("--c2c", nargs="*", type=int, help="需要输出的私聊消息的QQ号列表")
+parser.add_argument("--group", nargs="*", type=int, help="需要输出的群聊消息的群号列表")
+
 
 logging.basicConfig(
     level=logging.INFO,  # 设置默认日志级别
@@ -36,16 +42,19 @@ def run_single(query, task_type, path):
     logging.info(f"成功解析并写入{task_type}消息")
 
 def main():
+    args = parser.parse_args()
 
-    db_path = Path(argv[1])
+    db_path = Path(args.path)
     c2c_path, group_path = output_path(db_path)
 
     dbman = db.DatabaseManager(db_path)
 
-    run_single(dbman.c2c_messages, "私聊", c2c_path)
-    run_single(dbman.group_messages, "群聊", group_path)
+    c2c_filters = args.c2c
+    group_filters = args.group
+
     c2c_query = dbman.c2c_messages(c2c_filters)
     group_query = dbman.group_messages(group_filters)
+
     run_single(c2c_query, "私聊", c2c_path)
     run_single(group_query, "群聊", group_path)
 
