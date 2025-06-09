b77c5c5f654b725e81b7329c64a6ad91cb0fe931
Tealina <104749201+Tealina28@users.noreply.github.com>
2025-06-09 12:29:33 +0800

    feat(exporter): 为信息处理添加基类

--- export/txt_exporter/elements.py
+++ export/txt_exporter/elements.py
@@ -1,12 +1,6 @@
-from ast import literal_eval
-from functools import lru_cache
+from export.base_elements import *
+
 from json import loads
-from xml.etree.ElementTree import fromstring
-
-from humanize import naturalsize
-from unicodedata import category
-
-from emojis import emojis
 
 __all__ = [
     "Text",
@@ -22,60 +16,12 @@ __all__ = [
     "Feed",
 ]
 
-
-def readable_file_size(file_size):
-    """
-    Returns a human-readable file size.
-    """
-    return naturalsize(file_size, binary=True, format="%.2f") if file_size else None
-
-
-class Text:
-    def __init__(self, element):
-        self.text = element.text
-
-        self.content = self._get_content()
-
+class Text(BaseText):
     def _get_content(self):
         return "[文本]", self.text
 
 
-class Image:
-    def __init__(self, element):
-        self.text = element.imageText
-        self.file_name = element.fileName
-        self.readable_size = readable_file_size(element.fileSize)
-        self.file_path = element.imageFilePath
-        self.file_url = element.imageUrlOrigin
-
-        self.cache_path = self._get_cache_path(element.original, element.md5HexStr.hex().upper())
-
-        self.content = self._get_content()
-
-    @staticmethod
-    @lru_cache(maxsize=4096)
-    def _get_cache_path(original, md5HexStr):
-        def crc64(raw_str):
-            _crc64_table = [0] * 256
-            for i in range(256):
-                bf = i
-                for _ in range(8):
-                    bf = bf >> 1 ^ -7661587058870466123 if bf & 1 else bf >> 1
-                _crc64_table[i] = bf
-            value = -1
-            for char in raw_str:
-                value = _crc64_table[(ord(char) ^ value) & 255] ^ value >> 8
-            return value
-
-        # original == 0 指未发原图，图片存于chatraw
-        # original == 1 指发送原图，压缩后的图片存于chatimg,下载后原图存于chatraw
-        folder = "chatimg" if original else "chatraw"
-        raw_str = f"{folder}:{md5HexStr}"
-        crc64_value = crc64(raw_str)
-        file_name = f"Cache_{crc64_value:x}"
-
-        return f"/{folder}/{file_name[-3:]}/{file_name}"
-
+class Image(BaseImage):
     def _get_content(self):
         return "[图片]", "\n".join(
             part for part in [
@@ -86,26 +32,12 @@ class Image:
         )
 
 
-class File:
-    def __init__(self, element):
-        self.file_name = element.fileName
-        self.readable_size = readable_file_size(element.fileSize)
-
-        self.content = self._get_content()
-
+class File(BaseFile):
     def _get_content(self):
         return "[文件]", f"{self.file_name} {self.readable_size}"
 
 
-class Voice:
-    def __init__(self, element):
-        self.voice_text = element.voiceText
-        self.voice_len = element.voiceLen
-        self.file_name = element.fileName
-        self.readable_size = readable_file_size(element.fileSize)
-
-        self.content = self._get_content()
-
+class Voice(BaseVoice):
     def _get_content(self):
         return "[语音]", "\n".join(
             part for part in [
@@ -116,22 +48,7 @@ class Voice:
         )
 
 
-class Video:
-    def __init__(self, element):
-        self.formated_video_len = self._seconds_to_hms(element.videoLen)
-        self.file_name = element.fileName
-        self.readable_size = readable_file_size(element.fileSize)
-        self.path = element.videoPath
-
-        self.content = self._get_content()
-
-    @staticmethod
-    def _seconds_to_hms(seconds):
-        hours, remainder = divmod(seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-
-        return f"{hours:02}:{minutes:02}:{seconds:02}"
-
+class Video(BaseVideo):
     def _get_content(self):
         return "[视频]", "\n".join(
             part for part in [
@@ -141,89 +58,31 @@ class Video:
         )
 
 
-class Emoji:
-    def __init__(self, element):
-        self.emoji_id = element.emojiId
-        self.text = element.emojiText
-
-        self.content = self._get_content()
-
+class Emoji(BaseEmoji):
     def _get_content(self):
-        if not self.text:
-            self.text = emojis.get(self.emoji_id, "未知表情")
         return "[表情]", f"{self.text}-{self.emoji_id}"
 
 
-class Notice:
-    def __init__(self, element):
-        self.info = element.noticeInfo
-        self.info2 = element.noticeInfo2
-
-        self.content = self._get_content()
-
+class Notice(BaseNotice):
     def _get_content(self):
-        if not self.info and not self.info2:
-            return "[提示]", None
-        elif self.info:
+        return "[提示]", self._parse_info()
 
-            self.info = self.info.replace(r'\/', '/').replace('\u3000', ' ')
-            self.info = ''.join(char for char in self.info if category(char) not in ('Cf', 'Cc'))
-
-            root = fromstring(self.info)
-            texts = [
-                elem.get('txt')
-                for elem in root.findall('.//nor')
-                if elem.get('txt')
-            ]
-        elif self.info2:
-            info2_dict = literal_eval(self.info2.replace(r"\/", "/"))
-            texts = [item.get("txt", "")
-                     for item
-                     in info2_dict["items"]]
-
-        return "[提示]", " ".join(texts)
-
-
-class RedPacket:
-    def __init__(self, element):
-        self.prompt = element.redPacket.prompt
-        self.summary = element.redPacket.summary
-
-        self.content = self._get_content()
-
+class RedPacket(BaseRedPacket):
     def _get_content(self):
         return "[红包]", f"{self.summary} {self.prompt}"
 
 
-class Application:
-    def __init__(self, element):
-        self.raw = element.applicationMessage
-
-        self.content = self._get_content()
-
+class Application(BaseApplication):
     def _get_content(self):
         return "[应用消息]", loads(self.raw)["prompt"]
 
 
-class Call:
-    def __init__(self, element):
-        self.status = element.callStatus
-        self.text = element.callText
-
-        self.content = self._get_content()
-
+class Call(BaseCall):
     def _get_content(self):
         return "[通话]", f"{self.status}-{self.text}"
 
 
-class Feed:
-    def __init__(self, element):
-        self.title = element.feedTitle.text
-        self.feed_content = element.feedContent.text
-        self.url = element.feedUrl
-
-        self.content = self._get_content()
-
+class Feed(BaseFeed):
     def _get_content(self):
         return "[动态消息]", "\n".join(
             part for part in [
