9ce55ad8b6d3f25c972649791ebc14f0ffe44102
shingyu <shyu@staredges.cn>
2025-08-20 08:07:47 +0800

    fix(exporter): fix #18.

--- exporter/__init__.py
+++ exporter/__init__.py
@@ -2,4 +2,4 @@ import re
 
 def sanitize_filename(filename):
     illegal_chars = r'[<>:"/\\|?*]'
-    return re.sub(illegal_chars, '_', filename)
+    return re.sub(illegal_chars, '_', str(filename))
